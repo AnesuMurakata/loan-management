13159d54196a28c281b6daed7dea5d436184cc9e
AnesuMurakata <murac96@gmail.com>
2025-03-27 07:09:56 +0200

    add feedback while form is being submitted

--- web/src/components/AddPayment/AddPayment.tsx
+++ web/src/components/AddPayment/AddPayment.tsx
@@ -1,12 +1,16 @@
 import axios from "axios";
 import { useState } from "react";
+import { PulseLoader } from "react-spinners";
 
 const AddNewPayment = () => {
+  const [submittingData, setSubmittingData] = useState<boolean>(false);
   const [paymentLoanId, setPaymentLoanId] = useState<number | null>(null);
   const [paymentAmount, setPaymentAmount] = useState<number | null>(null);
 
   const submitPayment = (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
+    // Give user feedback that form is being submitted
+    setSubmittingData(true);
     axios({
       method: "POST",
       url: "http://localhost:2024/payments",
@@ -15,16 +19,19 @@ const AddNewPayment = () => {
       .then(() => {
         //Handle success
         console.log("Zvaita");
+        setSubmittingData(false);
       })
       .catch(() => {
         // Handle error
         console.log("Zvaramba");
+        setSubmittingData(false);
       });
   };
 
   return (
     <div>
       <h1>Add New Payment</h1>
+      {submittingData && <PulseLoader color='#fff' />}
       <form onSubmit={submitPayment}>
         <p>
           <label>Payment Loan Id</label>
